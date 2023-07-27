bab9bfd5f88cf967aab44526be2984925f86abfe
Parth Bhattad <bhattadparth20@gmail.com>
2023-07-27 20:48:39 +0530

    check

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -13,7 +13,7 @@ const Home = () => {
             <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
                 <div className="flex flex-col justify-center h-full mt-0">
                     <h2 className="text-4xl sm:text-7xl font-bold text-blue-400 mt-20">
-                        Welcome to my Portfolio! I am <Typed className=""   strings={[' a Student',' a Developer','a Quick Learner','a Tech Enthusiast']}
+                        Welcome to my Portfolio! I am <Typed className=""   strings={[' a Student',' a Developer','a Quick Learner','a Tech Enthusiast','IOT Enthusiast']}
                     typeSpeed={40} backSpeed={40} loop />
                     </h2>
                     <p className="text-gray-500 py-4 max-w-md">
